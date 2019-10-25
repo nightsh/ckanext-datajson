ed211c30138756b0a2ee05811f22c9d1f3bd8e46
Andrés Vázquez <andres@data99.com.ar>
2019-10-25 13:31:28 -0300

    fix path

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -3,4 +3,4 @@
 echo "TESTING ckanext-datajson"
 # nosetests --ckan --nologcapture --with-pylons=subdir/test.ini --reset-db --with-coverage --cover-package=ckanext.datajson --cover-inclusive --cover-erase --cover-tests
 
-nosetests --ckan --nologcapture --with-pylons=subdir/test-core.ini ckanext.datajson 
+nosetests --ckan --nologcapture --with-pylons=subdir/test.ini ckanext.datajson 
