3bfb7a001d66f0e36f02fb5844d6730147e72122
Andrés Vázquez <andres@data99.com.ar>
2019-10-29 12:57:56 -0300

    first new tests try

---
+++ ckanext/datajson/tests/mock_datajson_source.py
@@ -0,0 +1,77 @@
+from __future__ import print_function
+
+import os
+import json
+import re
+import copy
+import urllib
+
+import SimpleHTTPServer
+import SocketServer
+from threading import Thread
+
+PORT = 8998
+
+
+class MockDataJSONHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # test name is the first bit of the URL and makes CKAN behave
+        # differently in some way.
+        # Its value is recorded and then removed from the path
+        self.test_name = None
+        self.sample_datajson_file = None
+        self.samples_path = 'datajson-samples'
+        if self.path == 'https://www.archive.arm.gov/metadata/data.json':
+            self.sample_datajson_file = 'arm.data.json'
+            self.test_name = 'arm'
+        elif self.path == 'http://www.usda.gov/data.json':
+            self.sample_datajson_file = 'usda.gov.data.json'
+            self.test_name = 'usda'
+        elif self.path == 'http://some404/data.json':
+            self.test_name = 'e404'
+            self.respond('Not found', status=404)
+        elif self.path == 'http://some500/data.json':
+            self.test_name = 'e500'
+            self.respond('Error', status=500)
+        
+        if self.sample_datajson_file is not None:
+            self.respond_json_sample_file(file_path=self.sample_datajson_file)
+
+        if self.test_name == None:
+            self.respond('Mock DataJSON doesnt recognize that call', status=400)
+
+    def respond_json(self, content_dict, status=200):
+        return self.respond(json.dumps(content_dict), status=status,
+                            content_type='application/json')
+    
+    def respond_json_sample_file(self, file_path, status=200):
+        pt = os.path.join(self.samples_path, file_path)
+        data = json.load(open(pt, 'r'))
+        return self.respond(data, status=status,
+                            content_type='application/json')
+
+    def respond(self, content, status=200, content_type='application/json'):
+        self.send_response(status)
+        self.send_header('Content-Type', content_type)
+        self.end_headers()
+        self.wfile.write(content)
+        self.wfile.close()
+
+
+def serve(port=PORT):
+    '''Runs a CKAN-alike app (over HTTP) that is used for harvesting tests'''
+
+    # Choose the directory to serve files from
+    # os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+    #                      'mock_ckan_files'))
+
+    class TestServer(SocketServer.TCPServer):
+        allow_reuse_address = True
+
+    httpd = TestServer(("", PORT), MockDataJSONHandler)
+
+    print('Serving test HTTP server at port {}'.format(PORT))
+
+    httpd_thread = Thread(target=httpd.serve_forever)
+    httpd_thread.setDaemon(True)
+    httpd_thread.start()
---
+++ ckanext/datajson/tests/test_datajsonharvester.py
@@ -0,0 +1,92 @@
+import copy
+
+from nose.tools import assert_equal, assert_raises, assert_in
+import json
+from mock import patch, MagicMock, Mock
+from requests.exceptions import HTTPError, RequestException
+
+try:
+    from ckan.tests.helpers import reset_db, call_action
+    from ckan.tests.factories import Organization, Group
+except ImportError:
+    from ckan.new_tests.helpers import reset_db, call_action
+    from ckan.new_tests.factories import Organization, Group
+from ckan import model
+from ckan.plugins import toolkit
+
+from ckanext.harvest.harvesters.ckanharvester import ContentFetchError
+from ckanext.harvest.tests.factories import (HarvestSourceObj, HarvestJobObj,
+                                             HarvestObjectObj)
+from ckanext.harvest.tests.lib import run_harvest
+import ckanext.harvest.model as harvest_model
+from ckanext.harvest.harvesters.base import HarvesterBase
+from ckanext.harvest.harvesters.datajson.harvester_datajson import DataJsonHarvester
+
+import mock_datajson_source
+
+# Start data json sources server we can test harvesting against it
+mock_datajson_source.serve()
+
+
+class TestDataJSONHarvester(object):
+    @classmethod
+    def setup(cls):
+        reset_db()
+        harvest_model.setup()
+
+    def run_source(self, url):
+        source = HarvestSourceObj(url=url)
+        job = HarvestJobObj(source=source)
+
+        harvester = DataJsonHarvester()
+
+        # gather stage
+        print 'GATHERING'
+        obj_ids = harvester.gather_stage(job)
+        print job.gather_errors
+        print obj_ids
+        if len(obj_ids) == 0:
+            # nothing to see
+            return
+
+        harvest_object = harvest_model.HarvestObject.get(obj_ids[0])
+        print harvest_object.guid
+        print harvest_object.content
+
+        # fetch stage
+        print 'FETCHING'
+        result = harvester.fetch_stage(harvest_object)
+
+        print harvest_object.errors
+        print result
+
+        # fetch stage
+        print 'IMPORTING'
+        result = harvester.import_stage(harvest_object)
+
+        print harvest_object.errors
+        print result
+        print harvest_object.package_id
+        dataset = model.Package.get(harvest_object.package_id)
+        print dataset.name
+
+    def test_datason_usda(self):
+        url = 'https://www.archive.arm.gov/metadata/data.json'
+        self.run_source(url=url)
+    
+    def test_datason_arm(self):
+        url = 'http://www.usda.gov/data.json'
+        self.run_source(url=url)
+    
+    def test_datason_404(self):
+        url = 'http://some404/data.json'
+        self.run_source(url=url)
+        
+    def test_datason_500(self):
+        url = 'http://some500/data.json'
+        self.run_source(url=url)
+
+    def test_harvest(self):
+        results_by_guid = run_harvest(
+            url='http://localhost:%s/' % mock_ckan.PORT,
+            harvester=CKANHarvester())    
