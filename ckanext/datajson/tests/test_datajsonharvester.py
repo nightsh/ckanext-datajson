67c73703109664d79b043e41bb5b80424a981c0c
Andrés Vázquez <andres@data99.com.ar>
2019-10-30 13:12:20 -0300

    re fix tags

--- ckanext/datajson/tests/test_datajsonharvester.py
+++ ckanext/datajson/tests/test_datajsonharvester.py
@@ -84,7 +84,7 @@ class TestDataJSONHarvester(object):
         expected_title = "NCEP GFS: vertical profiles of met quantities at standard pressures, at Barrow"
         assert_equal(dataset.title, expected_title)
         tags = [tag.name for tag in dataset.get_tags()]
-        assert_in("ORNL", tags)
+        assert_in("ORNL".lower(), tags)
         assert_equal(len(dataset.resources), 1)
     
     def test_datason_usda(self):
@@ -93,7 +93,7 @@ class TestDataJSONHarvester(object):
         expected_title = "Department of Agriculture Congressional Logs for Fiscal Year 2014"
         assert_equal(dataset.title, expected_title)
         tags = [tag.name for tag in dataset.get_tags()]
-        assert_in("Congressional Logs", tags)
+        assert_in("Congressional Logs".lower(), tags)
         assert_equal(len(dataset.resources), 1)
     
     def test_datason_404(self):
