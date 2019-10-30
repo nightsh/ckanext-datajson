eb999b5f96472dd859fb53a6154eac441b028a96
Andrés Vázquez <andres@data99.com.ar>
2019-10-30 10:43:01 -0300

    fix path harvest DB again

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -3,7 +3,7 @@ set -e
 
 echo "This is travis-build.bash..."
 
-echo "Installing the packages that CKAN requires..."
+echo "\n\n**********\nInstalling the packages that CKAN requires..."
 sudo apt-get update -qq
 sudo apt-get install solr-jetty libcommons-fileupload-java libpq-dev postgresql postgresql-contrib
 
@@ -17,7 +17,7 @@ pip install setuptools -U
 # 	sudo apt-get install postgresql-9.1
 # fi
 
-echo "Installing CKAN and its Python dependencies..."
+echo "\n\n**********\nInstalling CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
 if [ $CKANVERSION == '2.8' ]
@@ -34,7 +34,7 @@ pip install -r dev-requirements.txt
 
 cd -
 
-echo "Setting up Solr..."
+echo "\n\n**********\nSetting up Solr..."
 # solr is multicore for tests on ckan master now, but it's easier to run tests
 # on Travis single-core still.
 # see https://github.com/ckan/ckan/issues/2972
@@ -44,18 +44,18 @@ sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 
 
-echo "Creating the PostgreSQL user and database..."
+echo "\n\n**********\nCreating the PostgreSQL user and database..."
 sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
 sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
 sudo -u postgres psql -c 'CREATE DATABASE datastore_test WITH OWNER ckan_default;'
 
-echo "Initialising the database..."
+echo "\n\n**********\nInitialising the database..."
 cd ckan
 paster db init -c test-core.ini
 cd -
 
 cd ..
-echo "Installing Harverter"
+echo "\n\n**********\nInstalling Harverter"
 git clone https://github.com/ckan/ckanext-harvest
 cd ckanext-harvest
 git checkout master
@@ -63,15 +63,17 @@ git checkout master
 python setup.py develop
 pip install -r pip-requirements.txt
 pip install -r dev-requirements.txt
-paster harvester initdb -c test.ini
+pwd
+ls -la
+paster harvester initdb -c ../ckan/test-core.ini
 cd -
 
-echo "Installing ckanext-datajson and its requirements..."
+echo "\n\n**********\nInstalling ckanext-datajson and its requirements..."
 cd ckanext-datajson
 pip install -r pip-requirements.txt
 python setup.py develop
 
-echo "Moving test.ini into a subdir..."
+echo "\n\n**********\nMoving test.ini into a subdir..."
 mkdir subdir
 mv test.ini subdir
 paster datajson_harvest initdb -c subdir/test.ini
